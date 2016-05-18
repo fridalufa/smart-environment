34e01de4a2106734e73c7f91828bc462fe1dba9a
fabreim <f.reimeier@fu-berlin.de>
2016-05-18 13:55:41 +0200

    Added root node initialization

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 #include "shell.h"
 #include "msg.h"
+#include "net/gnrc/rpl.h"
+#include "net/ipv6/addr.h"
 
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
@@ -9,11 +11,35 @@ static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 extern int _netif_config(int argc, char** argv);
 
 int rpl_init(void){
+	ipv6_addr_t addr;
+
 	// Message Queue for receiving potentially fast incoming networking packets
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
 
-    char* args[] = {"6","add","2001:db8::1"};
-    _netif_config(3, args);
+    // Create global interface 6
+    char* args[] = {"ifconfig","6","add","2001:db8::1"};
+    _netif_config(4, args);
+
+    // Check if interface exists
+    gnrc_ipv6_netif_t *entry = NULL;
+    kernel_pid_t iface_pid = 6;
+    entry = gnrc_ipv6_netif_get(iface_pid);
+
+    if (entry == NULL) {
+        puts("unknown interface specified");
+        return 1;
+    }
+
+    // RPL init
+	gnrc_rpl_init(6);
+
+	// Initialize root node
+	ipv6_addr_from_str(&addr, "2001:db8::1");
+	gnrc_rpl_root_init	(	0,
+							&addr,
+							true,
+							false 
+						);		
 
     return 0;
 }
