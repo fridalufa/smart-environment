8dea3675a92998521ab25a6b5bb71b4a7567f63b
Luca Keidel <info@lucakeidel.de>
2016-06-03 17:49:43 +0200

    Updated setup_net script.
    It is now possible to specify how many actors and sensors you want to create (-a option for actors and -s option for sensors respectively).

--- util/setup_net.sh
+++ util/setup_net.sh
@@ -1,4 +1,33 @@
-#! /bin/sh
+#! /bin/bash
+
+# number of actors
+ACTORS=2
+# number of sensors
+SENSORS=2
+
+# parse arguments
+while [[ $# > 1 ]]
+do
+opt="$1"
+
+case $opt in
+    -a|--actors)
+    ACTORS="$2"
+    shift
+    ;;
+    -s|--sensors)
+    SENSORS="$2"
+    shift
+    ;;
+    *)
+    echo "Unknown option: $1"
+    exit 1
+    ;;
+esac
+shift
+done
+
+TOTAL_NODES=$(($ACTORS + $SENSORS))
 
 # Check if tmux is installed
 if ! [ -x "$(command -v tmux)" ]; then
@@ -6,9 +35,10 @@ if ! [ -x "$(command -v tmux)" ]; then
   exit 1
 fi
 
-
 # determine paths of the smart environment project and RIOT
 SMART_ENVIRONMENT_PATH=$(readlink -f "$(dirname $(readlink -f $0))/..")
+SMART_ENV_ACTOR_PATH=$(readlink -f $SMART_ENVIRONMENT_PATH/actor)
+SMART_ENV_SENSOR_PATH=$(readlink -f $SMART_ENVIRONMENT_PATH/sensor)
 RIOT_PATH=$(readlink -f "$SMART_ENVIRONMENT_PATH/../RIOT")
 
 # tapsetup
@@ -20,27 +50,37 @@ if ! [ -e $TAPSETUP ]; then
 fi
 
 $TAPSETUP -d
-$TAPSETUP -c 3
+$TAPSETUP -c $TOTAL_NODES
 
-# make the smart environment project
-cd $SMART_ENVIRONMENT_PATH
+# make the actors
+cd $SMART_ENV_ACTOR_PATH
+make all
+# and the sensors
+cd $SMART_ENV_SENSOR_PATH
 make all
 
-# start sessions with the first session running the root node
 tmux new-session -d -s smartenv
 tmux rename-window 'Smart Environment'
-tmux select-window -t smartenv:0
-tmux send-keys "cd $SMART_ENVIRONMENT_PATH" 'C-m' 'make term PORT=tap0 MODE=root' 'C-m'
+# start actor sessions
+for i in $(seq 0 $(($ACTORS-1)))
+do
+    if [ $i -gt 0 ]; then
+        tmux split-window -h -t $(($i-1))
+    fi
+    tmux select-window -t smartenv:$i
+    tmux send-keys "cd $SMART_ENV_ACTOR_PATH" 'C-m' "make term PORT=tap$i" 'C-m'
+done
+# start sensor sessions
+for j in $(seq $ACTORS $(($TOTAL_NODES-1)))
+do
+    if [ $j -gt 0 ]; then
+        tmux split-window -h -t $(($j-1))
+    fi
+    tmux select-window -t smartenv:$j
+    tmux send-keys "cd $SMART_ENV_SENSOR_PATH" 'C-m' "make term PORT=tap$j" 'C-m'
+done
 
-# create and init second session
-tmux split-window -h -t 0
-tmux select-window -t smartenv:1
-tmux send-keys "cd $SMART_ENVIRONMENT_PATH" 'C-m' 'make term PORT=tap1' 'C-m'
-
-# create and init third session
-tmux split-window -h -t 0
-tmux select-window -t smartenv:2
-tmux send-keys "cd $SMART_ENVIRONMENT_PATH" 'C-m' 'make term PORT=tap2' 'C-m'
+# arrange sessions horizontally
 tmux select-layout even-horizontal
 
 # finally attach to the tmux session
