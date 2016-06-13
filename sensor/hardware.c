aa5594faff530692a248486638aaa6f079be7822
David Bohn <davbohn@googlemail.com>
2016-06-13 17:32:19 +0200

    Made debug output actual debug output

--- sensor/hardware.c
+++ sensor/hardware.c
@@ -2,6 +2,9 @@
 #include "hardware.h"
 #include "xtimer.h"
 
+#define ENABLE_DEBUG  (0)
+#include "debug.h"
+
 #if I2C_NUMOF > 0
 
 #include "tmp006.h"
@@ -19,7 +22,7 @@ int temp_sensor_init(void)
     // Initialize temperature sensor
     result = tmp006_init(&sensor, I2C_0, 0x40, TMP006_CONFIG_CR_AS2);
 
-    printf("Init result: %d\n", result);
+    DEBUG("Init result: %d\n", result);
 
 #endif
 
@@ -34,25 +37,25 @@ int temp_sensor_read(void)
     uint8_t drdy = 0;
 
     if (tmp006_set_active(&sensor)) {
-        puts("Measurement start failed.");
+        DEBUG("Measurement start failed.\n");
         return 1;
     }
 
     while (!drdy) {
         if (tmp006_read(&sensor, &rawvolt, &rawtemp, &drdy) == -1) {
-            puts("Failed to read temperature");
+            DEBUG("Failed to read temperature\n");
             xtimer_usleep(TMP006_CONVERSION_TIME);
             continue;
         }
     }
 
-    printf("Raw data T: %5d   V: %5d\n", rawtemp, rawvolt);
+    DEBUG("Raw data T: %5d   V: %5d\n", rawtemp, rawvolt);
     tmp006_convert(rawvolt, rawtemp,  &tamb, &tobj);
-    printf("Data Tabm: %d   Tobj: %d\n", (int)(tamb * 100), (int)(tobj * 100));
+    DEBUG("Data Tabm: %d   Tobj: %d\n", (int)(tamb * 100), (int)(tobj * 100));
 
     return (int)(tobj * 100);
 #else
-    puts("Random temperature value");
+    DEBUG("Random temperature value\n");
     return random_uint32_range(0, 40) * 100;
 #endif
 }
